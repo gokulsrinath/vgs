a8b25072add3b5f3ee43f38c18745f646d16c22e
gokulsrinath <30368312+gokulsrinath@users.noreply.github.com>
2017-08-21 13:41:31 +0530

    Create 8.c

---
+++ 8.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main()
+{
+int m, j, sum = 0;
+printf("Enter a positive integer: ");
+scanf("%d",&m);
+for(j=1; j <= n; ++j)
+{
+sum = sum+j;
+}
+printf("Sum = %d",sum);
+return 0;
+}
