7b59178740b04fbbb806c029b9cacdcc3e81c4de
gokulsrinath <30368312+gokulsrinath@users.noreply.github.com>
2017-08-21 14:00:03 +0530

    Create 14.c

---
+++ 14.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main()
+{
+    int j, m;
+    printf("Print odd numbers till: ");
+    scanf("%d", &m);
+    printf("All odd numbers from 1 to %d are: \n", n);
+    for(j=1; j<=m; j++)
+    {
+     if(j%2!=0)
+    {
+      printf("%d\n", m);
+     }
+    }
+
+    return 0;
+}
